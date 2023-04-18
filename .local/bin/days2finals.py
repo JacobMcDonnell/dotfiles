737a809d2fc5596b92dc9cc48efaa5cb57c69db2
Jacob McDonnell <jacob@simplelittledream.com>
2023-04-18 18:33:14 -0400

    modified tmux scroll buffer limit

--- .local/bin/days2finals.py
+++ .local/bin/days2finals.py
@@ -2,18 +2,22 @@ import datetime
 now = datetime.datetime.now()
 
 finalsDate = datetime.datetime(2023, 5, 1, 0, 0, 0, 0)
-daysLeft = finalsDate - now
+breakDate = datetime.datetime(2023, 3, 3, 15, 0, 0, 0)
+def getDays(date, name):
+    daysLeft = date - now
 
-weeks = daysLeft.days // 7
-days = daysLeft.days % 7
-minutes = daysLeft.seconds // 60
-hours = minutes // 60
-minutes %= 60
-seconds = daysLeft.seconds % 60
+    weeks = daysLeft.days // 7
+    days = daysLeft.days % 7
+    minutes = daysLeft.seconds // 60
+    hours = minutes // 60
+    minutes %= 60
 
-output = f"{weeks} weeks {days} days {hours} hours {minutes} minutes and {seconds} seconds until finals week"
+    if (weeks < 0):
+        return f"{name.upper()} WEEK"
+    return f"{weeks} weeks {days} days and {hours} hours until {name} week"
 
-if (weeks <=0):
-	output = "FINALS WEEK"
+output = f"{getDays(finalsDate, 'finals')}"
+#output += f"{getDays(breakDate, 'break')}"
+
 
 print(output)
