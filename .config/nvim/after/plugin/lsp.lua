4a903ca920b43184efb05a30c0bb79d21b56e0a9
Jacob McDonnell <jacob@jacobmcdonnell.com>
2024-03-26 21:43:28 -0400

    Updated neovim based on theprimagen

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,20 @@
+local lsp = require('lsp-zero')
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-n>'] = cmp_action.luasnip_jump_forward(),
+		['<C-p>'] = cmp_action.luasnip_jump_backward(),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+	})
+})
+
+require('lspconfig').clangd.setup({})
---
+++ .config/nvim/lua/jacob/init.lua
@@ -0,0 +1,3 @@
+require("jacob.remaps")
+require("jacob.plugins")
+require("jacob.set")
