f23a42df69157de582c2a953a9e041665656bd96
Jacob McDonnell <jacob@jacobmcdonnell.com>
2026-03-26 17:55:29 -0400

    fix: Vterm Custom Colors
    Set Vterm custom colors to fix issues with ncurses programs like
    menuconfig for linux.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -39,15 +39,33 @@
 (unless package-archive-contents
     (package-refresh-contents))
 
-;; (unless (package-installed-p 'acme-theme)
-;;     (package-install 'acme-theme))
-;;
-;; (use-package acme-theme
-;;   :config
-;;   (load-theme 'acme t))
+(unless (package-installed-p 'acme-theme)
+    (package-install 'acme-theme))
+
+(use-package acme-theme
+  :config
+  (load-theme 'acme t))
 
 (use-package vterm
     :ensure t)
+
+(custom-set-faces
+  '(vterm-color-black          ((t (:foreground "#e9e9ed" :background "#e9e9ed"))))
+  '(vterm-color-red            ((t (:foreground "#f52a65" :background "#f52a65"))))
+  '(vterm-color-green          ((t (:foreground "#587539" :background "#587539"))))
+  '(vterm-color-yellow         ((t (:foreground "#8c6c3e" :background "#8c6c3e"))))
+  '(vterm-color-blue           ((t (:foreground "#2e7de9" :background "#2e7de9"))))
+  '(vterm-color-magenta        ((t (:foreground "#9854f1" :background "#9854f1"))))
+  '(vterm-color-cyan           ((t (:foreground "#007197" :background "#007197"))))
+  '(vterm-color-white          ((t (:foreground "#6172b0" :background "#6172b0"))))
+  '(vterm-color-bright-black   ((t (:foreground "#a1a6c5" :background "#a1a6c5"))))
+  '(vterm-color-bright-red     ((t (:foreground "#f52a65" :background "#f52a65"))))
+  '(vterm-color-bright-green   ((t (:foreground "#587539" :background "#587539"))))
+  '(vterm-color-bright-yellow  ((t (:foreground "#8c6c3e" :background "#8c6c3e"))))
+  '(vterm-color-bright-blue    ((t (:foreground "#2e7de9" :background "#2e7de9"))))
+  '(vterm-color-bright-magenta ((t (:foreground "#9854f1" :background "#9854f1"))))
+  '(vterm-color-bright-cyan    ((t (:foreground "#007197" :background "#007197"))))
+  '(vterm-color-bright-white   ((t (:foreground "#3760bf" :background "#3760bf")))))
 
 (unless (package-installed-p 'magit)
   (package-install 'magit))
@@ -125,7 +143,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(acme-theme company evil-collection lsp-mode magit tree-sitter-langs
+                vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
